8e0f7afae0b9640853ed4f589c82598edf41c11f
PrinceMPS <prinmat88@gmail.com>
2025-02-28 10:42:59 +0800

    declare structs

--- candidates.c
+++ candidates.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef char Str20[21];
+typedef char Str128[129];
+typedef char Str32[33];
+typedef struct 
+{
+    Str20   First,
+            Middle,                                                                         
+            Last;
+}NameType;
+typedef struct 
+{
+int     month,
+        day,
+        year;
+}dateType;
+typedef struct 
+{
+    Str128 billName;
+    dateType datepassed;
+}billType;
+typedef struct candidateTag
+{
+    NameType Name;
+    dateType Birthday;
+    Str128 party;
+    Str32 position;
+
+
+}candidateType;
 
 
 int main()
@@ -9,6 +38,6 @@ int main()
 
 
 
-    
+
     return 0;
 }
